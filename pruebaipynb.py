d80e8457b0f0f5a860214decdcd2429f70cd2071
abelsenaperez <31017354+abelsenaperez@users.noreply.github.com>
2023-08-28 12:13:55 -0400

    Creado con Colaboratory

---
+++ pruebaipynb.py
@@ -0,0 +1,75 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "private_outputs": true,
+      "provenance": [],
+      "authorship_tag": "ABX9TyOgckzQbFts8/JkkErKSvUf",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/abelsenaperez/streamlit/blob/main/pruebaipynb.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "pip install streamlit"
+      ],
+      "metadata": {
+        "id": "KlPtjA_ul_UY"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "CwJLS3Qbjc0R"
+      },
+      "outputs": [],
+      "source": [
+        "import streamlit as st"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "import pandas as pd"
+      ],
+      "metadata": {
+        "id": "UlXy4-uUl6ys"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "st.title('hola mmg')"
+      ],
+      "metadata": {
+        "id": "Ct7dckn3mUQ9"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
