b0e1bb9492c0a9dfebbf6ec262920b3ab422fab4
abelsenaperez <31017354+abelsenaperez@users.noreply.github.com>
2023-09-01 11:46:16 -0400

    Creado con Colaboratory

---
+++ hola.py
@@ -0,0 +1,73 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "private_outputs": true,
+      "provenance": [],
+      "authorship_tag": "ABX9TyOVxbg1AB55KEG22tnVt39n",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/abelsenaperez/streamlit/blob/main/hola.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "kEqxAQCHabJB"
+      },
+      "outputs": [],
+      "source": [
+        "pip install streamlit"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "import streamlit as st"
+      ],
+      "metadata": {
+        "id": "MIkBfFEHameG"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "st.title('que hay de ti')"
+      ],
+      "metadata": {
+        "id": "aNna53ngarHK"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "Im0TqRq5axvD"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
