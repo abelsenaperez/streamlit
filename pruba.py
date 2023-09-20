3be02993d1516a31d379490f0078a96f3c2b9caf
abelsenaperez <31017354+abelsenaperez@users.noreply.github.com>
2023-09-20 14:59:13 -0400

    Add files via upload

--- pruba.py
+++ pruba.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pip
-pip.main(['install','openpyx1'])
-
-
-st.write("""
-# My first app
-Hello *world!*
-""")
-
-df = pd.read_csv("my_test.csv")
-
+ 
+df = pd.read_csv('data.csv')
+st.write(df)
